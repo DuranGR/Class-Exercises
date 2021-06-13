0e13f416440d12d68eba1aa6510d25536137db62
DuranGR <59778765+DuranGR@users.noreply.github.com>
2021-06-12 21:12:41 -0300

    Create Class_exercise_1.py

---
+++ Class_exercise_1.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, first, last, salary):
+        self.first = first
+        self.last = last
+        self.salary = salary
+        self.email = first + last + '@gmail.com'
+    def fullname(self):
+        print(self.first + ' ' + self.last)
